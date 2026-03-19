2f02cc67503657f72dc48a9abc981a57fa0fca55
Thalis Antunes <thalisantunessilva@gmail.com>
2026-03-19 03:02:28 -0300

    feat: implementead login

---
+++ src/pages/login/index.js
@@ -0,0 +1,12 @@
+import FormLogin from "../../components/FormLogin/index";
+import "./styles.css"; 
+
+function LoginAdmin() {
+  return (
+    <div className="login-admin-container">
+      <FormLogin />
+    </div>
+  );
+}
+
+export default LoginAdmin;
