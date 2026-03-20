1cf9a18c4e9a3e77a15471d4f5938498e5066632
Thalis Antunes <thalisantunessilva@gmail.com>
2026-03-20 19:32:44 -0300

    fix: settings vercel

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://44.202.78.174:4000";
+const API_URL = process.env.REACT_APP_API_URL || "https://api.cardialit.com.br/";
 
 console.log('🌐 API URL:', API_URL);
 
